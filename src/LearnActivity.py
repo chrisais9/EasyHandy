20173b27f311c9292659cea10595245935ee02f1
KooHyongMo <chrisais9@naver.com>
2019-12-06 12:37:52 +0900

    리소스 추가 및 테스트 반영

--- src/LearnActivity.py
+++ src/LearnActivity.py
@@ -1,4 +1,5 @@
-from PyQt5 import uic, QtWidgets, QtGui
+from PyQt5 import uic, QtWidgets, QtGui, QtCore
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QSize
 import sys
@@ -9,6 +10,7 @@ from string import ascii_uppercase
 class MyListWidget(QListWidget):
 
     def __init__(self):
+        super(MyListWidget, self).__init__()
         self.myListWidget = QListWidget()
 
         # self.setLayout()
@@ -24,11 +26,9 @@ class MyListWidget(QListWidget):
         print(self, "ListWidget", "You clicked: " + item.text())
 
 
-class MainWindow(QMainWindow): # QMainWindow
+class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-
-        print(self)
+        super(MainWindow, self).__init__()
 
         uic.loadUi('UI_Files/learn_activity.ui', self)
 
@@ -37,10 +37,19 @@ class MainWindow(QMainWindow): # QMainWindow
         # label의 값을 조정하기위해서는 데이터 타입을 PyQt5.QtCore.QSize로 넘겨줘야 됨.
         # movie.setScaledSize(s) # PyQt5.QtCore.QSize(360, 270)로 넘겨줘도 됨.
         # self.label_3.setGeometry(0, 0, 780, 441) 아니면 이거 사용해도될듯.. 늦게 서야 봄..
-        movie.setScaledSize(PyQt5.QtCore.QSize(360,270))
+        # movie.setScaledSize(PyQt5.QtCore.QSize(360,270))
 
-        self.label_imageView.setMovie(movie)
-        movie.start()
+        # self.label_imageView.setMovie(movie)
+        # movie.start()
+        file = self.loadImageFromFile("resource/alphabet/learn/A.png")
+        pixmap = QPixmap(file)
+        pixmap = pixmap.scaled(330, 215)
+        self.label_imageView.setPixmap(pixmap)
+
+        self.label_a.mousePressEvent = self.changeModeTo('a')
+
+        # self.listWidget_alpha_selecter.addItem('A')
+        # label.show()
 
         # myListWidget = MyListWidget()
         # myListWidget.setGeometry(170,330,260,190)
@@ -55,6 +64,14 @@ class MainWindow(QMainWindow): # QMainWindow
 
 
         # self.setCentralWidget(wg) # 반드시 필요함.
+
+    def changeModeTo(self, mode):
+        print('a')
+        return 'a'
+
+    def loadImageFromFile(self, path):
+        self.pixmap = QPixmap(path)
+        return self.pixmap
 
 
 
