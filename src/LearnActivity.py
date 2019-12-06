a82085e2714bdc276a855ce3959411f587091418
KooHyongMo <chrisais9@naver.com>
2019-12-06 15:12:30 +0900

    버튼 이벤트 모두 구현 완료 (버튼을 누르면 해당 알파벳을 리턴함)

--- src/LearnActivity.py
+++ src/LearnActivity.py
@@ -9,6 +9,7 @@ class Button(QPushButton):
     def __init__(self, text,callback):
         self.text = text
 
+
 class PicButton(QAbstractButton):
     def __init__(self, pixmap, parent=None):
         super(PicButton, self).__init__(parent)
@@ -29,7 +30,6 @@ class MainWindow(QMainWindow):
 
         uic.loadUi('UI_Files/learn_activity.ui', self)
 
-        movie = QtGui.QMovie("icons/dashAnimation.gif")
 
         # label의 값을 조정하기위해서는 데이터 타입을 PyQt5.QtCore.QSize로 넘겨줘야 됨.
         # movie.setScaledSize(s) # PyQt5.QtCore.QSize(360, 270)로 넘겨줘도 됨.
@@ -38,118 +38,112 @@ class MainWindow(QMainWindow):
 
         # self.label_imageView.setMovie(movie)
         # movie.start()
-        file = self.loadImageFromFile("resource/alphabet/learn/A.png")
-        pixmap = QPixmap(file)
-        pixmap = pixmap.scaled(330, 215)
-        self.label_imageView.setPixmap(pixmap)
-
-        self.label_a.mousePressEvent = self.changeModeTo('a')
 
 
         # self.button_A = PicButton(QPixmap("./resource/alphabet/learn/A.png"))
         self.button_A.setIcon(QtGui.QIcon("./resource/alphabet/button/A.png"))
-        self.button_A.setIconSize(QSize(30 ,30))
-        self.button_A.clicked.connect(Button.Clicked,'A')
+        self.button_A.setIconSize(QSize(30, 30))
+        self.button_A.clicked.connect(self.changeModeTo)
 
         self.button_B.setIcon(QtGui.QIcon("./resource/alphabet/button/B.png"))
         self.button_B.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'B')
+        self.button_B.clicked.connect(self.changeModeTo)
 
         self.button_C.setIcon(QtGui.QIcon("./resource/alphabet/button/C.png"))
         self.button_C.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'C')
+        self.button_C.clicked.connect(self.changeModeTo)
 
         self.button_D.setIcon(QtGui.QIcon("./resource/alphabet/button/D.png"))
         self.button_D.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'D')
+        self.button_D.clicked.connect(self.changeModeTo)
 
         self.button_E.setIcon(QtGui.QIcon("./resource/alphabet/button/E.png"))
         self.button_E.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'E')
+        self.button_E.clicked.connect(self.changeModeTo)
 
         self.button_F.setIcon(QtGui.QIcon("./resource/alphabet/button/F.png"))
         self.button_F.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'F')
+        self.button_F.clicked.connect(self.changeModeTo)
 
         self.button_G.setIcon(QtGui.QIcon("./resource/alphabet/button/G.png"))
         self.button_G.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'G')
+        self.button_G.clicked.connect(self.changeModeTo)
 
         self.button_H.setIcon(QtGui.QIcon("./resource/alphabet/button/H.png"))
         self.button_H.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'H')
+        self.button_H.clicked.connect(self.changeModeTo)
 
         self.button_I.setIcon(QtGui.QIcon("./resource/alphabet/button/I.png"))
         self.button_I.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'I')
+        self.button_I.clicked.connect(self.changeModeTo)
 
         self.button_J.setIcon(QtGui.QIcon("./resource/alphabet/button/J.png"))
         self.button_J.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'J')
+        self.button_J.clicked.connect(self.changeModeTo)
 
         self.button_K.setIcon(QtGui.QIcon("./resource/alphabet/button/K.png"))
         self.button_K.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'K')
+        self.button_K.clicked.connect(self.changeModeTo)
 
         self.button_L.setIcon(QtGui.QIcon("./resource/alphabet/button/L.png"))
         self.button_L.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'L')
+        self.button_L.clicked.connect(self.changeModeTo)
 
         self.button_M.setIcon(QtGui.QIcon("./resource/alphabet/button/M.png"))
         self.button_M.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'M')
+        self.button_M.clicked.connect(self.changeModeTo)
 
         self.button_N.setIcon(QtGui.QIcon("./resource/alphabet/button/N.png"))
         self.button_N.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'N')
+        self.button_N.clicked.connect(self.changeModeTo)
 
         self.button_O.setIcon(QtGui.QIcon("./resource/alphabet/button/O.png"))
         self.button_O.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'O')
+        self.button_O.clicked.connect(self.changeModeTo)
 
         self.button_P.setIcon(QtGui.QIcon("./resource/alphabet/button/P.png"))
         self.button_P.setIconSize(QSize(30, 30))
-        self.button_A.clicked.connect(Button.Clicked, 'P')
+        self.button_P.clicked.connect(self.changeModeTo)
 
         self.button_Q.setIcon(QtGui.QIcon("./resource/alphabet/button/Q.png"))
         self.button_Q.setIconSize(QSize(30, 30))
-        self.button_Q.clicked.connect(Button.Clicked, 'Q')
+        self.button_Q.clicked.connect(self.changeModeTo)
 
         self.button_R.setIcon(QtGui.QIcon("./resource/alphabet/button/R.png"))
         self.button_R.setIconSize(QSize(30, 30))
-        self.button_R.clicked.connect(Button.Clicked, 'R')
+        self.button_R.clicked.connect(self.changeModeTo)
 
         self.button_S.setIcon(QtGui.QIcon("./resource/alphabet/button/S.png"))
         self.button_S.setIconSize(QSize(30, 30))
-        self.button_S.clicked.connect(Button.Clicked, 'S')
+        self.button_S.clicked.connect(self.changeModeTo)
 
         self.button_T.setIcon(QtGui.QIcon("./resource/alphabet/button/T.png"))
         self.button_T.setIconSize(QSize(30, 30))
-        self.button_T.clicked.connect(Button.Clicked, 'T')
+        self.button_T.clicked.connect(self.changeModeTo)
 
         self.button_U.setIcon(QtGui.QIcon("./resource/alphabet/button/U.png"))
         self.button_U.setIconSize(QSize(30, 30))
-        self.button_U.clicked.connect(Button.Clicked, 'U')
+        self.button_U.clicked.connect(self.changeModeTo)
 
         self.button_V.setIcon(QtGui.QIcon("./resource/alphabet/button/V.png"))
         self.button_V.setIconSize(QSize(30, 30))
-        self.button_V.clicked.connect(Button.Clicked, 'V')
+        self.button_V.clicked.connect(self.changeModeTo)
 
         self.button_W.setIcon(QtGui.QIcon("./resource/alphabet/button/W.png"))
         self.button_W.setIconSize(QSize(30, 30))
-        self.button_W.clicked.connect(Button.Clicked, 'W')
+        self.button_W.clicked.connect(self.changeModeTo)
 
         self.button_X.setIcon(QtGui.QIcon("./resource/alphabet/button/X.png"))
         self.button_X.setIconSize(QSize(30, 30))
-        self.button_X.clicked.connect(Button.Clicked, 'X')
+        self.button_X.clicked.connect(self.changeModeTo)
 
         self.button_Y.setIcon(QtGui.QIcon("./resource/alphabet/button/Y.png"))
         self.button_Y.setIconSize(QSize(30, 30))
-        self.button_Y.clicked.connect(Button.Clicked, 'Y')
+        self.button_Y.clicked.connect(self.changeModeTo)
 
         self.button_Z.setIcon(QtGui.QIcon("./resource/alphabet/button/Z.png"))
         self.button_Z.setIconSize(QSize(30, 30))
-        self.button_Z.clicked.connect(Button.Clicked, 'Z')
+        self.button_Z.clicked.connect(self.changeModeTo)
 
         # self.pushButton.setGeometry(0,0,300,300)
         # self.widget.setIcon("./resource/alphabet/learn/A.png")
@@ -158,24 +152,14 @@ class MainWindow(QMainWindow):
         print(self.widget)
         print(type(self.widget))
 
-    def Clicked(self):
+
+    def changeModeTo(self):
         button = self.sender()
 
-        key = button.text()
-        if key == '=':
-            result = str(eval(self.display.text()))
-            self.display.setText(result)
-        elif key == '<-':
-            result = self.display.text()[0:-1]
-            self.display.setText(result)
-        elif key == 'C':
-            self.display.setText('')
-        else:
-            self.display.setText(self.display.text() + key)
-
-    def changeModeTo(self, mode):
-        print('a')
-        return 'a'
+        objName = button.objectName()
+        # Convert 'button_A' -> 'A'
+        key = objName[-1]
+        print(key)
 
     def loadImageFromFile(self, path):
         self.pixmap = QPixmap(path)
